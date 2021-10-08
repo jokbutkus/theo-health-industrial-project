4353ec28d6b1260f102f06df59cec67d66a82c5f
Andres <xxdrews@gmail.com>
2021-10-08 17:20:56 +0100

    Get endpoint for exercise retrieval

--- backend/src/main/java/com/theo/backend/controller/Controller.java
+++ backend/src/main/java/com/theo/backend/controller/Controller.java
@@ -1,31 +1,40 @@
 package com.theo.backend.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.theo.backend.json.request.LoginRequest;
 import com.theo.backend.json.response.LoginResponse;
+import com.theo.backend.model.Recording;
+import com.theo.backend.model.RecordingData;
 import com.theo.backend.model.User;
+import com.theo.backend.repositories.RecordingDataRepository;
+import com.theo.backend.repositories.RecordingRepository;
 import com.theo.backend.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class Controller {
+    Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 
     private final UserRepository userRepository;
+    private final RecordingRepository recordingRepository;
+    private final RecordingDataRepository recordingDataRepository;
 
     @GetMapping("/")
     public String index() {
         return "Welcome to Theo!";
     }
 
-    @PostMapping("/import-data")
-    public String importData() {
-        return "Thanks for importing data!";
+    @GetMapping("/exercise/{id}")
+    public String exerciseRetrieval(@PathVariable Long id) {
+        final Optional<Recording> recording = recordingRepository.findById(id);
+        final List<RecordingData> data = recordingDataRepository.findAllByRecording(recording);
+        return gson.toJson(data);
     }
 
     @PostMapping("/login")
