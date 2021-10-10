10ccf11adf2409bcc725eb45ab24494649c47d90
Andres <xxdrews@gmail.com>
2021-10-10 11:44:28 +0100

    Updated db queries

--- DB/db-queries.sql
+++ DB/db-queries.sql
@@ -4,20 +4,14 @@ DELETE RECORDING where true;
 DELETE USER where true;
 DELETE USER_ROLE where true;
 
-SELECT * FROM USER_ROLE;
-SELECT * FROM USER;
-SELECT * FROM ATHLETE_STAFF;
-SELECT * FROM RECORDING;
-SELECT * FROM RECORDINGDATA;
-
 
 INSERT INTO USER_ROLE (ID ,NAME ) VALUES ('1','trainer'),('2','athlete');
 
-INSERT INTO USER(ID ,NAME ,PASSWORD ,USERNAME, ROLE_ID) VALUES ('1', 'name1', 'password', 'username1', ' 1'), ('2', 'name2', 'password', 'username2', ' 2');
+INSERT INTO USER(ID ,NAME ,PASSWORD ,USERNAME, ROLE_ID) VALUES ('1', 'name1', 'password', 'username1', ' 1'), ('2', 'name2', 'password', 'username2', ' 2'), ('3', 'name3', 'password', 'username3', ' 2');
 
-INSERT INTO ATHLETE_STAFF (ID ,ATHLETE_ID ,STAFF_ID ) VALUES  ('1', '2', '1');
+INSERT INTO ATHLETE_STAFF (ID ,ATHLETE_ID ,STAFF_ID ) VALUES  ('1', '2', '1'), ('2', '3', '1');
 
-INSERT INTO RECORDING(RECORDINGID, ATHLETEID ) VALUES ('1','2');
+INSERT INTO RECORDING(RECORDINGID, ATHLETEID ) VALUES ('1','2'), ('2','3');
 
 INSERT INTO RECORDINGDATA(time, sensor1,sensor2,sensor3,sensor4, recordingID) SELECT * FROM CSVREAD('C:\\Users\\alvar\\Documents\\uni\\4th year\\IndustrialTeamProject\\datasets\\SensorTest-set2\\all-sensors.csv');
 
