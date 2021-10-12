159e02531af69dd4487053027d51e29c2b252f80
Mohammed Ubaid Tokaria <43134813+Mkt96@users.noreply.github.com>
2021-10-12 11:00:54 +0100

    Removed java files

--- frontend/public/Register.java
+++
@@ -1,7 +0,0 @@
-public class Register
-{
-    public int Id { get; set; }
-    public string Name { get, set; }
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
