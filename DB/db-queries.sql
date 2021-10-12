5c7bd07cd81a1159d397a6932e486f9f049bf3a6
Mohammed Ubaid Tokaria <43134813+Mkt96@users.noreply.github.com>
2021-10-12 10:11:47 +0100

    Update db-queries.sql

--- DB/db-queries.sql
+++ DB/db-queries.sql
@@ -1,9 +1,8 @@
 DELETE RECORDINGDATA where true;
 DELETE RECORDING where true;
+DELETE ATHLETE_STAFF where true;
 DELETE USER where true;
 DELETE USER_ROLE where true;
-DELETE ATHLETE_STAFF where true;
-
 
 INSERT INTO USER_ROLE (ID ,NAME ) VALUES ('1','trainer'),('2','athlete');
 
@@ -13,7 +12,7 @@ INSERT INTO ATHLETE_STAFF (ID ,ATHLETE_ID ,STAFF_ID ) VALUES  ('1', '2', '1'), (
 
 INSERT INTO RECORDING(RECORDINGID, DATE , ATHLETEID ) VALUES ('1', '2021-09-30','2'), ('2', '2021-10-01','2'),('3','2021-09-30','3');
 
-INSERT INTO RECORDINGDATA(time, sensor1,sensor2,sensor3,sensor4, recordingID) SELECT * FROM CSVREAD('C:\\Users\\alvar\\Documents\\uni\\4th year\\IndustrialTeamProject\\datasets\\SensorTest-set2\\all-sensors.csv');
+INSERT INTO RECORDINGDATA(time, sensor1,sensor2,sensor3,sensor4, recordingID) SELECT * FROM CSVREAD('C:\Users\alvar\Documents\uni\4th year\IndustrialTeamProject\datasets\SensorTest-set2\all-sensors.csv');
 
 SELECT * FROM USER_ROLE;
 SELECT * FROM USER;
