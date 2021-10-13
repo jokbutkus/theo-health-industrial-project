f4afa43d5f255cca576d903bcd88d171c1101e58
Mohammed Ubaid Tokaria <muktokaria@dundee.ac.uk>
2021-10-13 14:50:56 +0100

    Added CSS

--- frontend/src/components/pages/login.jsx
+++ frontend/src/components/pages/login.jsx
@@ -57,22 +57,47 @@ class Login extends Component {
           </button>
         </div>
         <h1>{this.state.aSession}</h1>
-        <div class="card-header">
-          <input type="text" id="username" placeholder="username" value={this.state.username} onChange={this.setUsername}/>
+        
+        <div class="container">
+          <h1 class="card-title" style={{ color: "#f36d21"}}>Login</h1>
         </div>
-        <div class="card-header">
-          <input type="text" id="password" placeholder="password" value={this.state.password} onChange={this.setPassword}/>
+
+        <div class="input-group mb-3 d-flex justify-content-center">
+          <div class="input-group-prepend">
+            <span class="input-group-text" style={{ borderColor: "ButtonShadow", marginRight: "5px" }}>Uesrname: </span>
+          </div>
+            <input style={{ borderRadius: "25px", padding: "3.5px 15px"}} type="text" id="username" placeholder="username" value={this.state.username} onChange={this.setUsername}/>
         </div>
+
+        <div class="input-group mb-3 d-flex justify-content-center">
+          <div class="input-group-prepend">
+            <span class="input-group-text" style={{ borderColor: "ButtonShadow", marginRight: "9px" }}>Password: </span>
+          </div>
+            <input style={{ borderRadius: "25px", padding: "3.5px 15px"}} type="text" id="password" placeholder="password" value={this.state.password} onChange={this.setPassword}/>
+        </div>
+
         <div>          
           <button
             class="btn btn-primary"
+            style={{
+              borderRadius: "25px",
+              padding: "10px 25px",
+              borderRadius: "50px",
+              margin: "10px 0 0 0",
+              position: "relative",
+              float: "right",
+              left: "-50%",
+              textAlign: "left"
+            }}
+
             onClick={() => {
               this.loginMethod(this.state.username, this.state.password);
             }}
             href="#"
-          >
+            >
             Login
-          </button></div>
+          </button>
+        </div>
       </div>
     );
   }
