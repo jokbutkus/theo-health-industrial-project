ddb5e0402caf707a98a09231b54ebaad808167e3
Andres <andresalo2011@gmail.com>
2021-10-14 13:57:52 +0100

    User dashboard, logout function and page redirection if user is logged in

--- frontend/src/components/pages/frontPage.jsx
+++ frontend/src/components/pages/frontPage.jsx
@@ -3,32 +3,44 @@ import "bootstrap/dist/css/bootstrap.css";
 import '../css/frontpage.css';
 
 const appStates = {
-    FrontPage: 1,
-    Login: 2,
-    Register: 3,
-    Dashboard: 4,
-  };
+  FrontPage: 1,
+  Login: 2,
+  Register: 3,
+  Dashboard: 4,
+};
 
 class FrontPage extends React.Component {
-    render() { 
-        return (
-          <div class="contentbody" style={{border:"0"}}>
-            <div class="row justify-content-left">
-              <div class="col-3">
-                <button class="button" style={{borderStyle:"solid", borderRadius:"100px"}} onClick={() => this.props.changeState(appStates.Login)}>
-                  Login
-                </button>
-              </div>
 
-              <div class="col-3">
-                <button class="button" style={{borderStyle:"solid", borderRadius:"100px"}} onClick={() => this.props.changeState(appStates.Register)}>
-                  Sign Up
-                </button>
-              </div>
-            </div>
-          </div>
-        );
+  skipLogin = () => {
+    if (localStorage.getItem("userID") != null) {
+      console.log("User is logged in")
+      console.log("User is logged in")
+      this.props.changeState(appStates.Dashboard)
     }
+  }
+
+  render() {
+    return (
+      <div class="contentbody" style={{ border: "0" }}>
+        {
+          this.skipLogin()
+        }
+        <div class="row justify-content-left">
+          <div class="col-3">
+            <button class="button" style={{ borderStyle: "solid", borderRadius: "100px" }} onClick={() => this.props.changeState(appStates.Login)}>
+              Login
+            </button>
+          </div>
+
+          <div class="col-3">
+            <button class="button" style={{ borderStyle: "solid", borderRadius: "100px" }} onClick={() => this.props.changeState(appStates.Register)}>
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default FrontPage;
